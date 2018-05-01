35128676d25e1e3401a508aa6672bec210576858
okazaki07 <clairsoleil27@yahoo.ne.jp>
2018-05-01 10:15:07 +0900

    0502

---
+++ HelloWorld/src/jp/co/internous/HelloWorld.java
@@ -0,0 +1,14 @@
+package jp.co.internous;
+
+import java.io.IOException;
+
+public class HelloWorld {
+
+	public static void main(String[] args)throws IOException {
+		System.out.println("入力してください");
+		String s = "123";
+		System.out.println(s);
+
+	}
+
+}
---
+++ Iphone/src/jp/co/internous/Android.java
@@ -0,0 +1,11 @@
+package jp.co.internous;
+
+public class Android {
+	public static void main(String[] args){
+		SmartPhone iphone = new SmartPhone();
+		iphone.play();
+		iphone.stop();
+		iphone.next();
+		iphone.back();
+	}
+}
