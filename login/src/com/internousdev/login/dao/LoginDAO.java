14f614779b8482eaa8a7724269a1ad72e77bd3af
shunm-999 <shunmiyazaki812@yahoo.co.jp>
2018-05-25 11:15:51 +0900

    5/25

--- login/src/com/internousdev/login/dao/LoginDAO.java
+++ login/src/com/internousdev/login/dao/LoginDAO.java
@@ -1,37 +1,36 @@
 package com.internousdev.login.dao;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.internousdev.login.dto.LoginDTO;
-import com.internousdev.login.util.DBConnector;
+import com.internousdev.util.DBConnector;
 
 public class LoginDAO {
 		public LoginDTO select(String name,String password) throws SQLException{
-			LoginDTO dto=new LoginDTO();
-			com.internousdev.login.util.DBConnector db = new DBConnector();
-			Connection con = db.getConnection();
-			String sql="select * from user where user_name=? and password=?";
+				LoginDTO dto=new LoginDTO();
+				DBConnector db = new DBConnector();
+				Connection con = db.getConnection();
+				String sql="select * from user where user_name=? and password=?";
 
-			try{
+		try{
+				PreparedStatement ps = con.prepareStatement(sql);
+				ps.setString(1,name);
+				ps.setString(2, password);
+				ResultSet rs=ps.executeQuery();
+				if(rs.next()) {
 
-					PreparedStatement ps = con.prepareStatement(sql);
-					ps.setString(1,name);
-					ps.setString(2,password);
-					ResultSet rs=ps.executeQuery();
+				dto.setName(rs.getString("user_name"));
+				dto.setPassword(rs.getString("password"));
 
-					if(rs.next()){
-							dto.setName(rs.getString("user_name"));
-							dto.setPassword(rs.getString("password"));
-					}
-					}catch(SQLException e){
-					 e.printStackTrace();
-					}finally{
-						con.close();
-					}
-				return dto;
-}
+				}
+				} catch (SQLException e) {
+				e.printStackTrace();
+				} finally {
+				con.close();
+				}
+			return dto;
+		}
 
 }
